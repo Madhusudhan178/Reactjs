51a0e4d92235eb465b063ecd995ab98fafcaef97
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-08-12 20:33:18 +0530

    inital commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function f1()
 {
+    let arr=[50,60,200,4,7,50]
+    // console.log(arr.lastIndexOf(50));//5
+    // console.log(arr.lastIndexOf(200))//2 
+    // console.log(typeof(arr))
+    let x=arr.toString();
+    console.log(typeof(x))
+    let z=arr.join(" ");
+    arr.push(10,20)
+    console.log(z)
     
 }
+f1()
