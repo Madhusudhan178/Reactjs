bc059f3921ba53a43055b3c49e9aec9a2854d201
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-08-13 19:20:45 +0530

    inital commit

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ function f1()
     let z=arr.join(" ");
     arr.push(10,20)
     console.log(z)
+    data={
+        id:1,
+        name:"pooja"
+    }
+    let a=Object.entries(data)
+    console.log(a);
+    console.log(typeof(a));
+    console.log(Object.fromEntries(a));
+    let m=document.querySelectorAll(".c")
+    console.log(m)
+for(let i=0;i<m.length;i++)
+{
+    console.log(m[i])
+}
+    let c=document.getElementsByClassName("c")
+    console.log(c)
+    let array=Array.from(c);
+    console.log(array);
+    console.log(array[1]);
+    
     
 }
 f1()
