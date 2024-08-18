36d9a4b95d16a9b2257c2b00e8d9fa978fd0ae95
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-08-18 08:46:28 +0530

    iital commit

--- mock.js
+++ mock.js
@@ -1,31 +1,33 @@
-let a = document.querySelector("button");
-let im = document.querySelector("body");
-let arr = ["app-link-background.svg", "google-play-badge.svg", "Homebackground.webp", "logo_knightbite.png"]
+// let a = document.querySelector("button");
+// let im = document.querySelector("body");
+// let arr = ["app-link-background.svg", "google-play-badge.svg", "Homebackground.webp", "logo_knightbite.png"]
 
 
-function f1() {
-    // for(let i=0;i<arr.length;i++)
-    //     {
-    //         f=Math.trunc(Math.random()*4);
-    //     }
-    //     console.log(arr[f])
-    //     console.log((typeof(arr[f])))
-    //     im.style.backgroundImage="url(./"+arr[f]+")";
-    let r = Math.trunc(Math.random() * 250);
-    let g = Math.trunc(Math.random() * 250);
-    let b = Math.trunc(Math.random() * 250);
-    im.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+// function f1() {
+//     // for(let i=0;i<arr.length;i++)
+//     //     {
+//     //         f=Math.trunc(Math.random()*4);
+//     //     }
+//     //     console.log(arr[f])
+//     //     console.log((typeof(arr[f])))
+//     //     im.style.backgroundImage="url(./"+arr[f]+")";
+//     let r = Math.trunc(Math.random() * 250);
+//     let g = Math.trunc(Math.random() * 250);
+//     let b = Math.trunc(Math.random() * 250);
+//     im.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 
-
-}
-// let x;
-// console.log(x)
-
-// let z= ()=>
-// {
-//     var x=10;
-//     console.log(x);
 
 // }
-// z()
-// console.log(z())
+// // let x;
+// // console.log(x)
+
+// // let z= ()=>
+// // {
+// //     var x=10;
+// //     console.log(x);
+
+// // }
+// // z()
+// // console.log(z())1
+let a;
+console.log(a);
