fe0e050d5500e527e3cca897f0ddaeaf07d0036b
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-08-19 17:29:15 +0530

    inital commit

--- mock.js
+++ mock.js
@@ -28,10 +28,46 @@
 
 // // }
 // // z()
-// // console.log(z())1
-let a=[10,20,30,40];
-console.log(...a);
-a.map((z)=>
-{
-    console.log(z)
-})
+// // console.log(z()))
+
+
+// let a=[10,20,30,40];
+// console.log(...a);
+// a.map((z)=>
+// {
+//     console.log(z)
+// })
+// let x=[100,200,300,400]
+// let [q,w,...e]=x;
+// console.log(q);
+// console.log(w);
+// console.log(e);
+
+
+
+let o={id:1,name:"pooja"};
+let o2={age:30}
+//console.log(...o,...o);//requires iteratable objects
+let z={...o,...o2};
+console.log(z)
+// let s=setInterval(function f1()
+// {
+//     let d =new Date();
+//     console.log(d)
+// },2000)
+
+//addEventListener("click",function)
+
+let arr=[10,20,10,30,10,40,50]
+function  removeduplicate(ar){
+    return arr.filter((item,index)=>
+        ar.indexOf(item)===index);
+}
+console.log(arr)
+let as=removeduplicate(arr)
+console.log(as);
+arr.filter((item,index)=>{
+    console.log("this item is:"+item)
+    console.log("this index is:"+index)
+    arr.indexOf(item)===index});
+;
